57c650ef9e7593c3ee79cc3e086f8d513862de71
Ali Alqrinawi <104166368+AliAlqrinawi@users.noreply.github.com>
2023-04-03 15:21:48 +0300

    category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+
+    public function scopeChangeStatus()
+    {
+        if($this->status == "ACTIVE"){
+            $this->update(['status' => 'NACTIVE']);
+        }else{
+            $this->update(['status' => 'ACTIVE']);
+        }
+    }
 }
