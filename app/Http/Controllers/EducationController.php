1a5f530c812efac4fce5c552b6bfb937fd9509c5
Ali Alqrinawi <104166368+AliAlqrinawi@users.noreply.github.com>
2023-04-03 13:22:09 +0300

    Education

---
+++ app/Http/Controllers/EducationController.php
@@ -0,0 +1,196 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Education;
+use Illuminate\Http\Request;
+use Yajra\DataTables\DataTables;
+
+class EducationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        if ($request->ajax()) {
+            $data = Education::get();
+            return DataTables::of($data)
+                ->addIndexColumn()
+                ->addColumn('status', function ($row) {
+                    if ($row->status == 'ACTIVE') {
+                        $status = '<button class="modal-effect btn btn-sm btn-success" style="margin: 5px" id="status" data-id="' . $row->id . '" ><i class=" icon-check"></i></button>';
+                    } else {
+                        $status = '<button class="modal-effect btn btn-sm btn-danger" style="margin: 5px" id="status" data-id="' . $row->id . '" ><i class=" icon-check"></i></button>';
+                    }
+                    return $status;
+                })
+                ->addColumn('action', function ($row) {
+                    $btn = '<button class="modal-effect btn btn-sm btn-info"  style="margin: 5px" id="showModalEditEducation" data-id="' . $row->id . '"><i class="las la-pen"></i></button>';
+                    $btn = $btn . '<button class="modal-effect btn btn-sm btn-danger" id="showModalDeleteEducation" data-name="' . $row->title_en . '" data-id="' . $row->id . '"><i class="las la-trash"></i></button>';
+                    return $btn;
+                })
+                ->rawColumns([
+                    'status' => 'status',
+                    'action' => 'action',
+                ])
+                ->make(true);
+        }
+        return view('dashboard.views-dash.education.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $educationData = $request->all();
+        $validator = Validator($educationData, [
+            'title_en' => 'required|string|min:3|max:255',
+            'Learn_resource_en' => 'required|string|min:3|max:255',
+            'description_en' => 'required|string|min:3|max:255',
+            'year_range' => 'required|string|min:3|max:255',
+        ]);
+        if (!$validator->fails()) {
+            $ads = Education::create($educationData);
+                $response = [
+                    'message' => 'Added successfully',
+                    'status' => 200,
+                ];
+                return ControllersService::responseSuccess($response);
+        } else {
+            $response = [
+                'message' => $validator->getMessageBag()->first(),
+                'status' => 400,
+            ];
+            return ControllersService::responseErorr($response);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $ads = Education::find($id);
+        if($ads){
+            $response = [
+                'message' => 'Found Data',
+                'status' => 200,
+                'data' => $ads
+            ];
+            return ControllersService::responseSuccess($response);
+        }else{
+            $response = [
+                'message' => 'Not Found Data',
+                'status' => 400,
+            ];
+            return ControllersService::responseErorr($response);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $educationData = $request->all();
+        $validator = Validator($educationData, [
+            'title_en' => 'required|string|min:3|max:255',
+            'Learn_resource_en' => 'required|string|min:3|max:255',
+            'description_en' => 'required|string|min:3|max:255',
+            'year_range' => 'required|string|min:3|max:255',
+        ]);
+        if (!$validator->fails()) {
+            $ads = Education::find($id)->update($educationData);
+                $response = [
+                    'message' => 'Added successfully',
+                    'status' => 200,
+                ];
+                return ControllersService::responseSuccess($response);
+        } else {
+            $response = [
+                'message' => $validator->getMessageBag()->first(),
+                'status' => 400,
+            ];
+            return ControllersService::responseErorr($response);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $education = Education::find($id);
+        if($education){
+            $education->delete();
+            $response = [
+                'message' => 'Deleted successfully',
+                'status' => 200,
+            ];
+            return ControllersService::responseSuccess($response);
+        }else{
+            $response = [
+                'message' => 'Not Found Data',
+                'status' => 400,
+            ];
+            return ControllersService::responseErorr($response);
+        }
+    }
+
+    public function status($id)
+    {
+        $education = Education::find($id);
+        if($education){
+            $education->changeStatus();
+            $response = [
+                'message' => 'Updated successfully',
+                'status' => 200,
+            ];
+            return ControllersService::responseSuccess($response);
+        }else{
+            $response = [
+                'message' => 'Not Found Data',
+                'status' => 400,
+            ];
+            return ControllersService::responseErorr($response);
+        }
+    }
+}
